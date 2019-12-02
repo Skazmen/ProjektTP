9ee65e6e3644346c41d264e176648ec11bc390e8
Skazmen <skaziuwielkisz69@gmail.com>
2019-12-02 15:48:21 +0100

    moreGui

--- GOO/src/main/java/Menu/BoardGui.java
+++ GOO/src/main/java/Menu/BoardGui.java
@@ -23,8 +23,8 @@ public class BoardGui extends JFrame implements ActionListener {
     private Scanner in;
     private PrintWriter out;
     private CountDownLatch sync = new CountDownLatch(1); //for 'sendToServer' to wait for 'out' to be inicjalized;
-    private JButton surrenderButton;
-    private JLabel backGroundLabel, stateLabel;
+    private JButton surrenderButton, giveUpTurn, confrimMove;
+    private JLabel backGroundLabel, stateLabel, backGroundLabel1;
     private boolean first = true;
 
     BoardGui() {
@@ -38,30 +38,46 @@ public class BoardGui extends JFrame implements ActionListener {
         sendToServer(MessagesClient.SURRENDER);
 
         setSize(1366, 768);
-        setTitle("Go game - Loading");
+        setTitle("Go game");
         setLayout(null);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setResizable(false);
 
         //Back button
-        surrenderButton = new JButton("leave");
+        surrenderButton = new JButton("surrender");
         surrenderButton.setBounds(1180, 660, 180, 30);
         add(surrenderButton);
         surrenderButton.setForeground(Color.white);
         surrenderButton.setContentAreaFilled(false);
-        surrenderButton.setToolTipText("Click here to leave session");
+        surrenderButton.setToolTipText("Click here to surrender session");
         surrenderButton.setFont(new Font("SansSerif", Font.BOLD, 20));
         surrenderButton.addActionListener(this);
-        setResizable(false);
 
-        //background
-        backGroundLabel = new JLabel(new ImageIcon("images/loading.jpg"));
-        backGroundLabel.setOpaque(true);
-        backGroundLabel.setBounds(0, 0, 1366, 768);
-        add(backGroundLabel);
+        giveUpTurn = new JButton("Give up turn");
+        giveUpTurn.setBounds(950, 400, 200, 30);
+        add(giveUpTurn);
+        giveUpTurn.setForeground(Color.white);
+        giveUpTurn.setContentAreaFilled(false);
+        giveUpTurn.setToolTipText("Click here to give up move");
+        giveUpTurn.setFont(new Font("SansSerif", Font.BOLD, 20));
+        giveUpTurn.addActionListener(this);
+
+        confrimMove = new JButton("Confrim move");
+        confrimMove.setBounds(950, 300, 200, 30);
+        add(confrimMove);
+        confrimMove.setForeground(Color.white);
+        confrimMove.setContentAreaFilled(false);
+        confrimMove.setToolTipText("Click here to confrim move");
+        confrimMove.setFont(new Font("SansSerif", Font.BOLD, 20));
+        confrimMove.addActionListener(this);
+
+        backGroundLabel1 = new JLabel(new ImageIcon("images/tlo.jpg"));
+        backGroundLabel1.setOpaque(true);
+        backGroundLabel1.setBounds(0, 0, 1366, 768);
+        add(backGroundLabel1);
 
         stateLabel = new JLabel();
-        stateLabel.setBounds(0, 0, 1366, 768);
+        stateLabel.setBounds(200, 30, 500, 500);
         stateLabel.setOpaque(false);
         stateLabel.setHorizontalAlignment(JLabel.CENTER);
         add(stateLabel);
@@ -81,19 +97,19 @@ public class BoardGui extends JFrame implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent e) {
         Object source = e.getSource();
-        remove(backGroundLabel);
+        remove(backGroundLabel1);
 
         if (first) {
-            backGroundLabel = new JLabel(new ImageIcon("images/loading.jpg"));
-            backGroundLabel.setOpaque(true);
-            backGroundLabel.setBounds(0, 0, 1366, 768);
+            backGroundLabel1 = new JLabel(new ImageIcon("images/tlo.jpg"));
+            backGroundLabel1.setOpaque(true);
+            backGroundLabel1.setBounds(0, 0, 1366, 768);
         } else {
-            backGroundLabel = new JLabel(new ImageIcon("images/loading.jpg"));
-            backGroundLabel.setOpaque(true);
-            backGroundLabel.setBounds(0, 0, 1366, 768);
+            backGroundLabel1 = new JLabel(new ImageIcon("images/tlo.jpg"));
+            backGroundLabel1.setOpaque(true);
+            backGroundLabel1.setBounds(0, 0, 1366, 768);
         }
 
-        add(backGroundLabel);
+        add(backGroundLabel1);
         first = !first;
         repaint();
 
@@ -104,9 +120,19 @@ public class BoardGui extends JFrame implements ActionListener {
             menu.setVisible(true);
             this.setVisible(false);
         }
+
+        if (source == giveUpTurn) {
+            sendToServer(MessagesClient.GIVE_UP_MOVE);
+        }
+
+        if (source == confrimMove) {
+            sendToServer(MessagesClient.MADE_MOVE);
+        }
+
+
     }
 
-    private void connectToServer() { //pokaz mi miejsce  wktórym dostjaesz info z backendu że np gracz ywkonał ruch, albo w ktorym miejscu to sie wyswie
+    private void connectToServer() {
         new Thread(new Runnable() {
             @Override
             public void run() {
@@ -122,39 +148,39 @@ public class BoardGui extends JFrame implements ActionListener {
                                 System.out.println("Your color is Black");
 								stateLabel.setText("Your color is Black");
 								stateLabel.setOpaque(true);
-								//todo -> nie pamiętam jak, zrób sleep na 4s i wtedy odekmentuj poniższą linię
-								//stateLabel.setOpaque(false);
+                                Thread.sleep(4000);
+								stateLabel.setOpaque(false);
 
                                 break;
                             case SET_COLOR_WHITE:
 								stateLabel.setText("Your color is White");
 								stateLabel.setOpaque(true);
-								//todo -> nie pamiętam jak, zrób sleep na 4s i wtedy odekmentuj poniższą linię
-								//stateLabel.setOpaque(false);
+                                Thread.sleep(4000);
+								stateLabel.setOpaque(false);
 
                                 System.out.println("Your color is White");
                                 break;
                             case WRONG_MOVE:
                             	stateLabel.setText("The move you tride to make is not allowed");
                             	stateLabel.setOpaque(true);
-                            	//todo -> nie pamiętam jak, zrób sleep na 4s i wtedy odekmentuj poniższą linię
-								//stateLabel.setOpaque(false);
+                                Thread.sleep(4000);
+								stateLabel.setOpaque(false);
 
                                 System.out.println("The move you tride to make is not allowed");
                                 break;
                             case UPDATE_BOARD:
 								stateLabel.setText("Your move was good, here is updated board");
 								stateLabel.setOpaque(true);
-								//todo -> nie pamiętam jak, zrób sleep na 4s i wtedy odekmentuj poniższą linię
-								//stateLabel.setOpaque(false);
+                                Thread.sleep(4000);
+								stateLabel.setOpaque(false);
 
                                 System.out.println("Your move was good, here is updated board");
                                 break;
                             case END_GAME:
 								stateLabel.setText("The game has ended and PlayerX won");
 								stateLabel.setOpaque(true);
-								//todo -> nie pamiętam jak, zrób sleep na 4s i wtedy odekmentuj poniższą linię
-								//stateLabel.setOpaque(false);
+                                Thread.sleep(4000);
+								stateLabel.setOpaque(false);
 
                                 System.out.println("The game has ended and PlayerX won");
                                 break;
@@ -163,6 +189,8 @@ public class BoardGui extends JFrame implements ActionListener {
                 } catch (ConnectException | UnknownHostException e) {
                     System.out.println("Cannot connect to  server - run server first");
                 } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
             }
